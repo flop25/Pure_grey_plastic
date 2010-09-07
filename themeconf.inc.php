0804ff911be501f63387cdce97a135e8ec6f9362
flop25 <flop25@piwigo.org>
2010-09-07 13:12:33 +0000

    version 1.0.0 for pwg 2.1.x
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_grey_plastic@6865 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Theme Name: Pure_grey_plastic
+Version: 1.0.0
+Description: a subtle  mix of grey with a 3d effect
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=446
+Author: flop25
+Author URI: http://www.planete-flop.fr/site.php?acc=file
+*/
+
+$themeconf = array(
+  'name'         => 'Pure_grey_plastic',
+  'parent'        => 'Pure_default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+);
+
+?>
